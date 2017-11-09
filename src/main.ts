20ef77019c80b6e8565a0cf04b0dacdbf6600663
MOUILLERON Cedric <mouilleron.cedric@gmail.com>
2017-11-09 09:04:47 +0100

    using options for feed authentification

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,40 @@
 import * as GTT from 'gdax-trading-toolkit';
-import { GDAXFeed } from 'gdax-trading-toolkit/build/src/exchanges';
-import { TradeMessage } from 'gdax-trading-toolkit/build/src/core';
+import program  = require('commander');
+import { GDAX_API_URL, GDAX_WS_FEED, GDAXFeed, GDAXFeedConfig } from 'gdax-trading-toolkit/build/src/exchanges';
+import { StreamMessage, TradeMessage } from 'gdax-trading-toolkit/build/src/core';
+import { getSubscribedFeeds } from 'gdax-trading-toolkit/build/src/factories/gdaxFactories';
 
+// create the default logger
 const logger = GTT.utils.ConsoleLoggerFactory();
-const products: string[] = ['BTC-EUR', 'ETH-EUR', 'LTC-EUR'];
-const tallies: any = {};
-products.forEach((product: string) => {
-    tallies[product] = {};
-});
 
-GTT.Factories.GDAX.FeedFactory(logger, products)
+// specify the PRODUCT to connect
+const products: string[] = ['BTC-EUR', 'ETH-EUR', 'LTC-EUR'];
+
+program
+    .option('--api [value]', 'API url', 'https://api.gdax.com')
+    .option('--ws [value]', 'WSI url', 'https://ws-feed.gdax.com')
+    .option('-p --product [value]', 'The GDAX product to query', 'BTC-USD')
+    .parse(process.argv);
+
+const options: GDAXFeedConfig = {
+    logger: logger,
+    auth: {
+        key: program.key || process.env.GDAX_KEY,
+        secret: program.secret || process.env.GDAX_SECRET,
+        passphrase: program.passphrase || process.env.GDAX_PASSPHRASE}, // use public feed
+    channels: null,
+    wsUrl: GDAX_WS_FEED,
+    apiUrl: GDAX_API_URL
+};
+
+getSubscribedFeeds(options, products)
 .then((feed: GDAXFeed) => {
-        feed.on('data', (msg: any) => {
-            if (!(msg as any).productId) {
-                logger.log('error', 'No ProductId defined');
-            } else {
-                const msgType = (msg as any).type;
-                if (msgType === 'trade') {
-                    const tradeMsg = (msg as TradeMessage);
-                    logger.log('debug', 'receive trade message');
-                    logger.log('info', 'Place <' + tradeMsg.productId + '>, price => ' + tradeMsg.price);
-                }
+        feed.on('data', (msg: StreamMessage) => {
+            // console.log(JSON.stringify(msg));
+            if (msg.type === 'trade') {
+                const tradeMsg = (msg as TradeMessage);
+                logger.log('info', 'Place <' + tradeMsg.productId + '>, price => ' + tradeMsg.price);
+                console.log(JSON.stringify(msg));
             }
         });
 }).catch((err: Error) => {
