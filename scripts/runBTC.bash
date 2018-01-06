e1ef05839dace047e99d88e9f61ca18db7e87ce1
Cédric Mouilleron <mouilleron.cedric@gmail.com>
2018-01-06 23:45:31 +0100

    Added 3 scripts to start application

---
+++ scripts/runBTC.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+__REAL_SCRIPTDIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
+__REAL_PROJECTDIR=$(dirname ${__REAL_SCRIPTDIR})
+
+echo "Running scripts"
+cd ${__REAL_PROJECTDIR}
+npm run run_ts_BTC
